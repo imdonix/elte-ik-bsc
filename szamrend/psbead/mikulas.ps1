605f27d3b98cfd9b3756a317cb12120b236c69a5
Magyar Tamás <tamas.donix@gmail.com>
2019-11-27 07:59:13 +0100

    szamrend powershell bead added

---
+++ szamrend/psbead/mikulas.ps1
@@ -0,0 +1,21 @@
+﻿if($args.Length -eq 2)
+{
+    if((Test-Path -PathType Leaf -Path $args[0]) -and (Test-Path -PathType Leaf -Path $args[1]))
+    {
+        $database = Get-Content($args[0])
+        $file = Get-Content($args[1])
+        foreach ($line in $database)
+        {
+            $splitted = $line.Split(' ');
+            Write-Output($file.Replace("<nev>",$splitted[0]).Replace("<cim>",$splitted[1]).Replace("<idopont>",$splitted[0]))
+        }
+    }
+    else
+    {
+        Write-Host("Bad files")
+    }
+}
+else
+{
+    Write-Host("Bad params")
+}
