91bbe952f73fed64ddddef33d0c3ce8d24a46099
imdonix <tamas.donix@gmail.com>
2019-11-28 12:23:17 +0100

    eprogalap gyak 1,3 added

---
+++ progalap/gyak/elso.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double a = 10.;
+
+    return 0;
+}
---
+++ progalap/gyak/third.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+    int i = 0;
+    while(lapok[i] != lap)
+    {
+        ++i;
+    }
+    ertek = ertekek[i];
+
+
+    cout << oszto;
+
+    return 0;
+}
