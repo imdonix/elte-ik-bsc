f41ef97202505ca832ac63a4f8b64ae78a3ed8e9
Magyar Tamás <tamas.donix@gmail.com>
2019-12-08 15:13:52 +0100

    szamrend psbead

---
+++ szamrend/psbead/count.ps1
@@ -0,0 +1,42 @@
+﻿$ee=0
+$eo=0
+$oe=0
+$oo=0
+$i = 0;
+
+foreach ($line in (Get-Content $args[0]))
+{
+    if (($i % 2) -eq 0)
+    {
+        foreach ($number in $line.Split(' '))
+        {
+            if (($number % 2) -eq 0)
+            {
+                $ee += $number
+            }
+            else
+            {
+                $eo += $number
+            }
+        }
+    }
+    else
+    {
+        foreach ($number in $line.Split(' '))
+        {
+            if (($number % 2) -eq 0)
+            {
+                $oe += $number
+            }
+            else
+            {
+                $oo += $number
+            }
+        }
+    }
+    $i++;
+}
+Write-Host Paros sorok paros szamainak osszege: $ee
+Write-Host Paros sorok paratlan szamainak osszege: $eo
+Write-Host Paratlan sorok paros szamainak osszege: $oe
+Write-Host Paratlan sorok paratlan szamainak osszege: $oo
