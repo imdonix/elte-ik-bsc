8082c1552c213123f6017212ffc3ac5918fa8ebd
Magyar Tamás <tamas.donix@gmail.com>
2020-03-03 08:12:11 +0100

    c# gyak3 added

---
+++ 2019-20-2/prognyelvek (C#)/gyak3/Szinezo/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Szinezo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                button1.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+                if (colorDialog1.ShowDialog() == DialogResult.OK)
+                    BackColor = colorDialog1.Color;
+        }
+    }
+}
