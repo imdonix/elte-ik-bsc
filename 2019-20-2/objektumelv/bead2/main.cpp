324c283eed278be27ff18caeced2a20405455d94
Magyar Tamás <tamas.donix@gmail.com>
2020-04-09 10:03:03 +0200

    oep bead2 finalized (doc,test)

--- 2019-20-2/objektumelv/bead2/main.cpp
+++ 2019-20-2/objektumelv/bead2/main.cpp
@@ -37,6 +37,9 @@ bool is_angler_catch_catfish(const string& src)
     return l;
 }
 
+#define NORMAL_MODE
+#ifdef NORMAL_MODE
+
 int main()
 {
 
@@ -73,3 +76,82 @@ int main()
 
     return 0;
 }
+
+#else
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+//sum first task
+
+TEST_CASE("first task empty file", "t0.txt") 
+{
+
+    CHECK(count_catfish("t0.txt") == 0);
+}
+
+TEST_CASE("first task 1 angler, true", "t2.txt") 
+{
+    CHECK(count_catfish("t2.txt") == 1);
+}
+
+TEST_CASE("first task 1 angler, false", "t13.txt") 
+{
+    CHECK(count_catfish("t13.txt") == 0);
+}
+
+TEST_CASE("first task more anglers, first one meets the requirements", "tt3.txt") 
+{
+    CHECK(count_catfish("tt3.txt") == 3);
+}
+
+TEST_CASE("first task more anglers, third one meets the requirements", "tt5.txt") 
+{
+    CHECK(count_catfish("tt5.txt") == 5);
+}
+
+TEST_CASE("first task more anglers", "tt6.txt") 
+{
+    CHECK(count_catfish("tt6.txt") == 5);
+}
+
+TEST_CASE("first task more anglers, few meets the requirements", "t6.txt") 
+{
+    CHECK(count_catfish("t6.txt") == 4);
+}
+
+// file check
+
+TEST_CASE("no catch", "t8.txt") 
+{
+    ContestEnor tt("t8.txt");
+    tt.first();
+    CHECK_FALSE(tt.current().catfish_catch);
+}
+
+// sum second task
+
+TEST_CASE("empty file", "t0.txt") 
+{
+    CHECK_FALSE(is_angler_catch_catfish("t0.txt"));
+}
+
+TEST_CASE("1 angler", "t2.txt") 
+{
+    CHECK(is_angler_catch_catfish("t2.txt"));
+}
+
+TEST_CASE("more angler more contests", "t4.txt") 
+{
+    CHECK(is_angler_catch_catfish("t4.txt"));
+}
+
+TEST_CASE("all angler is skillful", "t4.txt") 
+{
+    CHECK(is_angler_catch_catfish("t4.txt"));
+}
+
+TEST_CASE("last angler is skillful", "t5.txt") 
+{
+    CHECK(is_angler_catch_catfish("t5.txt"));
+}
+#endif
