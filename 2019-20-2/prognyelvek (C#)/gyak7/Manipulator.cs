34c3b0f3bf423221a9593ecb7d1f7c88c4350b32
Magyar Tamás <tamas.donix@gmail.com>
2020-04-14 12:43:44 +0200

    c# gyak 7 added

---
+++ 2019-20-2/prognyelvek (C#)/gyak7/Manipulator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KepSzintelenito
+{
+    static class Manipulator
+    {
+        public static Bitmap Manipulate(Bitmap bmp, int R, int G, int B)
+        {
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    Color pixel = bmp.GetPixel(x, y);
+                    bmp.SetPixel(x, y, Color.FromArgb((pixel.R + pixel.G + pixel.B) / 3, pixel.G, pixel.B));
+
+                    //if (pixel.R > R || pixel.G > G || pixel.B > B)
+                    //{
+                    //    int colorValue = (pixel.R + pixel.G + pixel.B) / 3;
+                    //    bmp.SetPixel(x, y, Color.FromArgb(colorValue, colorValue, colorValue));
+                    //}
+                }
+            }
+            return bmp;
+        }
+    }
+}
---
+++ 2019-20-2/prognyelvek (C#)/gyak7/WithoutConn.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp38
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void eMPBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.eMPBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.dataSet1);
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dataSet1.EMP' table. You can move, or remove it, as needed.
+            this.eMPTableAdapter.Fill(this.dataSet1.EMP);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+}
