7e9dd2ceb59a1fd0b8ad40834c2bc5c495cadc97
Magyar Tamás <tamas.donix@gmail.com>
2020-04-19 20:00:49 +0200

    c# bead init

---
+++ 2019-20-2/prognyelvek (C#)/beadando/ExamApp/MainForm.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace ExamApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.MainPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.AuthorLabel = new System.Windows.Forms.Label();
+            this.ContentPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.InformationPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.ExamsPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.SelfPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.MainPanel.SuspendLayout();
+            this.ContentPanel.SuspendLayout();
+            this.InformationPanel.SuspendLayout();
+            this.SelfPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.ColumnCount = 1;
+            this.MainPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.MainPanel.Controls.Add(this.TitleLabel, 0, 0);
+            this.MainPanel.Controls.Add(this.AuthorLabel, 0, 2);
+            this.MainPanel.Controls.Add(this.ContentPanel, 0, 1);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(0, 0);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.RowCount = 3;
+            this.MainPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.MainPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 85F));
+            this.MainPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.MainPanel.Size = new System.Drawing.Size(915, 426);
+            this.MainPanel.TabIndex = 0;
+            this.MainPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Font = new System.Drawing.Font("Lucida Sans", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLabel.Location = new System.Drawing.Point(393, 7);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(129, 27);
+            this.TitleLabel.TabIndex = 0;
+            this.TitleLabel.Text = "ExamApp";
+            // 
+            // AuthorLabel
+            // 
+            this.AuthorLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.AuthorLabel.AutoSize = true;
+            this.AuthorLabel.Location = new System.Drawing.Point(385, 409);
+            this.AuthorLabel.Name = "AuthorLabel";
+            this.AuthorLabel.Size = new System.Drawing.Size(145, 12);
+            this.AuthorLabel.TabIndex = 1;
+            this.AuthorLabel.Text = "Magyar Tamás  @ 2020";
+            // 
+            // ContentPanel
+            // 
+            this.ContentPanel.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.ContentPanel.ColumnCount = 2;
+            this.ContentPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.ContentPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.ContentPanel.Controls.Add(this.InformationPanel, 0, 0);
+            this.ContentPanel.Controls.Add(this.ExamsPanel, 1, 0);
+            this.ContentPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ContentPanel.Location = new System.Drawing.Point(3, 45);
+            this.ContentPanel.Name = "ContentPanel";
+            this.ContentPanel.RowCount = 1;
+            this.ContentPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.ContentPanel.Size = new System.Drawing.Size(909, 356);
+            this.ContentPanel.TabIndex = 2;
+            // 
+            // InformationPanel
+            // 
+            this.InformationPanel.ColumnCount = 1;
+            this.InformationPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.InformationPanel.Controls.Add(this.SelfPanel, 0, 0);
+            this.InformationPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.InformationPanel.Location = new System.Drawing.Point(4, 4);
+            this.InformationPanel.Name = "InformationPanel";
+            this.InformationPanel.RowCount = 2;
+            this.InformationPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 22.22222F));
+            this.InformationPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 77.77778F));
+            this.InformationPanel.Size = new System.Drawing.Size(537, 348);
+            this.InformationPanel.TabIndex = 0;
+            // 
+            // ExamsPanel
+            // 
+            this.ExamsPanel.AutoScroll = true;
+            this.ExamsPanel.ColumnCount = 1;
+            this.ExamsPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.ExamsPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ExamsPanel.Location = new System.Drawing.Point(548, 4);
+            this.ExamsPanel.Name = "ExamsPanel";
+            this.ExamsPanel.RowCount = 2;
+            this.ExamsPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 185F));
+            this.ExamsPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 185F));
+            this.ExamsPanel.Size = new System.Drawing.Size(357, 348);
+            this.ExamsPanel.TabIndex = 1;
+            // 
+            // SelfPanel
+            // 
+            this.SelfPanel.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.OutsetPartial;
+            this.SelfPanel.ColumnCount = 1;
+            this.SelfPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.SelfPanel.Controls.Add(this.label1, 0, 2);
+            this.SelfPanel.Controls.Add(this.NameLabel, 0, 1);
+            this.SelfPanel.Controls.Add(this.label2, 0, 0);
+            this.SelfPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SelfPanel.Location = new System.Drawing.Point(3, 3);
+            this.SelfPanel.Name = "SelfPanel";
+            this.SelfPanel.RowCount = 3;
+            this.SelfPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.SelfPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.SelfPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.SelfPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.SelfPanel.Size = new System.Drawing.Size(531, 71);
+            this.SelfPanel.TabIndex = 1;
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Font = new System.Drawing.Font("Lucida Sans Typewriter", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameLabel.Location = new System.Drawing.Point(6, 30);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(284, 15);
+            this.NameLabel.TabIndex = 0;
+            this.NameLabel.Text = "Hosszunevű János Tamás Magyar Tamás";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "RNYR2F";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Bejelentkezve mint,";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(915, 426);
+            this.Controls.Add(this.MainPanel);
+            this.Font = new System.Drawing.Font("Lucida Sans Typewriter", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(464, 234);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Exam";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MainPanel.ResumeLayout(false);
+            this.MainPanel.PerformLayout();
+            this.ContentPanel.ResumeLayout(false);
+            this.InformationPanel.ResumeLayout(false);
+            this.SelfPanel.ResumeLayout(false);
+            this.SelfPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel MainPanel;
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.Label AuthorLabel;
+        private System.Windows.Forms.TableLayoutPanel ContentPanel;
+        private System.Windows.Forms.TableLayoutPanel InformationPanel;
+        private System.Windows.Forms.TableLayoutPanel ExamsPanel;
+        private System.Windows.Forms.TableLayoutPanel SelfPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
