a7be98a20929a007719949867bd4abc9364d856a
Magyar Tamás <tamas.donix@gmail.com>
2020-11-02 10:11:40 +0100

    dimod zh1 added

--- 2020-21-1/dimod/seged/lnko.js
+++ 2020-21-1/dimod/seged/lnko.js
@@ -1,3 +1,5 @@
+const print = (x,y) => console.log(`${y} = ${Math.floor(y/x)} * ${x} + ${y%x}`);
+
 let x = parseInt(process.argv[2]);
 let y = parseInt(process.argv[3]);
 
@@ -6,6 +8,10 @@ if(x > y)
 
 while(y%x != 0)
 {
-    console.log(`${y} = ${Math.floor(y/x)} * ${x} + ${y%x}`);
+    print(x,y);
     [x,y] = [y%x, x];
 }
+
+print(x,y)
+
+
