18d1d3be95934e622c826e72dad508b066f6984f
Magyar Tamás <tamas.donix@gmail.com>
2021-03-13 19:26:29 +0100

    added visitor template

--- src/hu/elte/madtycoon/core/World.java
+++ src/hu/elte/madtycoon/core/World.java
@@ -1,11 +1,20 @@
 package hu.elte.madtycoon.core;
 
+import hu.elte.madtycoon.objects.Entities.Visitor;
+import hu.elte.madtycoon.objects.Entity;
+import hu.elte.madtycoon.objects.GameObject;
 import hu.elte.madtycoon.render.SpriteRenderBuffer;
+import hu.elte.madtycoon.utils.Vector2F;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class World
 {
     public static int DEFAULT_ENTRANCE_COST = 50;
     public static int DEFAULT_START_MONEY = 200;
+
+    private final List<Entity> entities;
 
     private int money;
     private int entranceCost;
@@ -14,16 +23,23 @@ public class World
     {
         money = DEFAULT_START_MONEY;
         entranceCost = DEFAULT_ENTRANCE_COST;
+        entities = new LinkedList<Entity>();
+        start();
+    }
+
+    private void start()
+    {
+        entities.add(Visitor.CreateNewVisitor(this, new Vector2F(1,1)));
     }
 
     public void update(float dt)
     {
-        //TODO
+        for(final GameObject obj : entities) obj.update(dt);
     }
 
     public void render(SpriteRenderBuffer buffer)
     {
-        //TODO
+        for(final GameObject obj : entities) obj.render(buffer);
     }
 
     public int getMoney()
--- src/hu/elte/madtycoon/objects/Entities/Visitor.java
+++ src/hu/elte/madtycoon/objects/Entities/Visitor.java
@@ -3,7 +3,10 @@ package hu.elte.madtycoon.objects.Entities;
 import hu.elte.madtycoon.core.World;
 import hu.elte.madtycoon.objects.Entity;
 import hu.elte.madtycoon.render.AnimatedSprite;
+import hu.elte.madtycoon.render.AnimationResource;
 import hu.elte.madtycoon.utils.Vector2F;
+
+import java.awt.image.BufferedImage;
 
 public class Visitor extends Entity
 {
@@ -13,8 +16,21 @@ public class Visitor extends Entity
         super(world, sprite, position);
     }
 
-    public static Visitor CreateNewVisitor()
+    @Override
+    public void update(float dt) {
+        super.update(dt);
+
+        sprite.setState("walk");
+        position = position.add(Vector2F.DOWN.mul(dt));
+        System.out.println(position);
+    }
+
+    public static Visitor CreateNewVisitor(World world, Vector2F position)
     {
-        return null;
+        BufferedImage[] idle = AnimationResource.Instance.get("visitor_idle");
+        BufferedImage[] walk = AnimationResource.Instance.get("visitor_walk");
+        AnimatedSprite anim = new AnimatedSprite("idle", idle, 0.25f);
+        anim.addState("walk", walk);
+        return new Visitor(world, anim, position);
     }
 }
