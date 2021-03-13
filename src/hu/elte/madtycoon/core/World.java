337648abe12d4fba927db6c1b54c951bf4dbb89f
Magyar Tamás <tamas.donix@gmail.com>
2021-03-13 18:59:38 +0100

    added world atributes

--- src/hu/elte/madtycoon/core/World.java
+++ src/hu/elte/madtycoon/core/World.java
@@ -4,6 +4,18 @@ import hu.elte.madtycoon.render.SpriteRenderBuffer;
 
 public class World
 {
+    public static int DEFAULT_ENTRANCE_COST = 50;
+    public static int DEFAULT_START_MONEY = 200;
+
+    private int money;
+    private int entranceCost;
+
+    public World()
+    {
+        money = DEFAULT_START_MONEY;
+        entranceCost = DEFAULT_ENTRANCE_COST;
+    }
+
     public void update(float dt)
     {
         //TODO
@@ -12,5 +24,20 @@ public class World
     public void render(SpriteRenderBuffer buffer)
     {
         //TODO
+    }
+
+    public int getMoney()
+    {
+        return money;
+    }
+
+    public int getEntranceCost()
+    {
+        return entranceCost;
+    }
+
+    public void setEntranceCost(int entranceCost)
+    {
+        this.entranceCost = entranceCost;
     }
 }
