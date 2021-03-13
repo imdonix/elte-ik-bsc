5bf4ef5d29eb4a30e6d0c5c76fdf8137cee0aa88
Magyar Tamás <tamas.donix@gmail.com>
2021-03-13 17:18:01 +0100

    Added SpriteRenderBuffer

---
+++ src/hu/elte/madtycoon/render/SpriteRenderBuffer.java
@@ -0,0 +1,40 @@
+package hu.elte.madtycoon.render;
+
+
+import hu.elte.madtycoon.core.Engine;
+import hu.elte.madtycoon.utils.MinHeap;
+import hu.elte.madtycoon.utils.Vector2F;
+import hu.elte.madtycoon.utils.Vector2I;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class SpriteRenderBuffer extends MinHeap<LayeredSprite>
+{
+    public SpriteRenderBuffer(int capacity) {
+        super(capacity);
+    }
+
+    public void draw(Graphics g)
+    {
+        while(!isEmpty())
+        {
+            LayeredSprite next = rem();
+            Vector2I pixelPos = VectorToScreenPosition(next.pos);
+            g.drawImage(next.sprite, pixelPos.x, pixelPos.y, null);
+        }
+    }
+
+    private Vector2I VectorToScreenPosition(Vector2F v)
+    {
+        int baseX = (int) Math.floor(v.x);
+        int baseY = (int) Math.floor(v.y);
+        int inX = (int) (v.x - baseX) * Engine.BLOCK_SIZE;
+        int inY = (int) (v.y - baseY) * Engine.BLOCK_SIZE;
+        return new Vector2I(baseX * Engine.BLOCK_SIZE + inX, baseY * Engine.BLOCK_SIZE + inY);
+    }
+}
