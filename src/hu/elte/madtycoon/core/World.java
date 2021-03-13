9ba220ebb6b9efb179fb71f847c1dadf7a90c592
Magyar Tamás <tamas.donix@gmail.com>
2021-03-13 17:18:42 +0100

    added world template

---
+++ src/hu/elte/madtycoon/core/World.java
@@ -0,0 +1,16 @@
+package hu.elte.madtycoon.core;
+
+import hu.elte.madtycoon.render.SpriteRenderBuffer;
+
+public class World
+{
+    public void update(float dt)
+    {
+        //TODO
+    }
+
+    public void render(SpriteRenderBuffer buffer)
+    {
+        //TODO
+    }
+}
