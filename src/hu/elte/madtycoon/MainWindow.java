9729682e076774721ad0a1395276deaab8743b04
Magyar Tamás <tamas.donix@gmail.com>
2021-03-14 13:58:23 +0100

    menu resources moved to Resources

--- src/hu/elte/madtycoon/MainWindow.java
+++ src/hu/elte/madtycoon/MainWindow.java
@@ -1,5 +1,7 @@
 package hu.elte.madtycoon;
 
+import hu.elte.madtycoon.core.Resources;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -50,22 +52,17 @@ public class MainWindow extends JFrame {
         exit.setBorderPainted(false);
         exit.addActionListener(e -> System.exit(0));
 
-        try {
-            play.setIcon(new ImageIcon(ImageIO.read(new File("res/menu_play_button.png"))));
-            settings.setIcon(new ImageIcon(ImageIO.read(new File("res/menu_settings_button.png"))));
-            credits.setIcon(new ImageIcon(ImageIO.read(new File("res/menu_credits_button.png"))));
-            exit.setIcon(new ImageIcon(ImageIO.read(new File("res/menu_exit_button.png"))));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        play.setIcon(new ImageIcon(Resources.Instace.menuPlayButton));
+        settings.setIcon(new ImageIcon(Resources.Instace.menuSettingsButton));
+        credits.setIcon(new ImageIcon(Resources.Instace.menuCreditsButton));
+        exit.setIcon(new ImageIcon(Resources.Instace.menuExitButton));
 
         this.setPreferredSize(screenSize);
-            Image img = Toolkit.getDefaultToolkit().getImage("res/menu_bg.jpg");
             this.setContentPane(new JPanel() {
                 @Override
                 public void paintComponent(Graphics g) {
                     super.paintComponent(g);
-                    g.drawImage(img, 0, 0, this);
+                    g.drawImage(Resources.Instace.menuBackGroundImage, 0, 0, this);
                 }
             });
         panel.add(play);
--- src/hu/elte/madtycoon/core/Resources.java
+++ src/hu/elte/madtycoon/core/Resources.java
@@ -10,16 +10,35 @@ public class Resources
 {
     public static Resources Instace = new Resources();
 
+    public BufferedImage menuBackGroundImage;
+    public BufferedImage menuPlayButton;
+    public BufferedImage menuSettingsButton;
+    public BufferedImage menuCreditsButton;
+    public BufferedImage menuExitButton;
+
     public BufferedImage gameBackGroundImage;
     public BufferedImage gameHudImage;
-
 
     private Resources() {}
 
     public void load() throws IOException
     {
+        loadMenu();
+        loadGame();
+    }
+
+    private void loadMenu() throws IOException
+    {
+        menuBackGroundImage = ImageIO.read(new File("res/menu_bg.jpg"));
+        menuPlayButton = ImageIO.read(new File("res/menu_play_button.png"));
+        menuSettingsButton = ImageIO.read(new File("res/menu_settings_button.png"));
+        menuCreditsButton = ImageIO.read(new File("res/menu_credits_button.png"));
+        menuExitButton = ImageIO.read(new File("res/menu_exit_button.png"));
+    }
+
+    private void loadGame() throws IOException
+    {
         gameBackGroundImage = ImageIO.read(new File("res/main_window_bg.jpg"));
         gameHudImage = ImageIO.read(new File("res/main_window_hud.jpg"));
     }
-
 }
