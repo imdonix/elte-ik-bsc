2d158472e8468696ba5f62475b838c49a4e7edc1
Magyar Tamás <tamas.donix@gmail.com>
2021-03-31 15:22:04 +0200

    Removed not used import

--- src/hu/elte/madtycoon/ui/core/PreviewFrame.java
+++ src/hu/elte/madtycoon/ui/core/PreviewFrame.java
@@ -1,23 +1,7 @@
 package hu.elte.madtycoon.ui.core;
 
-import hu.elte.madtycoon.core.Resources;
-import hu.elte.madtycoon.core.World;
-import hu.elte.madtycoon.objects.Building;
-import hu.elte.madtycoon.objects.GameObject;
-import hu.elte.madtycoon.ui.components.PreviewActionComponent;
-import hu.elte.madtycoon.utils.Utils;
-import javafx.scene.shape.Box;
-
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseWheelEvent;
-import java.awt.event.MouseWheelListener;
-import java.io.File;
-import java.io.IOException;
-import java.util.Random;
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 
 
 public class PreviewFrame extends JFrame
