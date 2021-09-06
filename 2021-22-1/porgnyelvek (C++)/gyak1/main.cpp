1cc7a63935fc10a2a9a0f1914e76d1244bc6e04b
Magyar Tamás <tamas.donix@gmail.com>
2021-09-06 13:43:26 +0200

    cpp gyak1 added

---
+++ 2021-22-1/porgnyelvek (C++)/gyak1/main.cpp
@@ -0,0 +1,23 @@
+#include
+
+int main()
+{
+
+    int i = 10;
+
+    b = (c = 2);
+
+    if( i = 0 )
+    {
+        std::cout << 1 << std::endl
+    }
+    else
+    {
+        std::cout << 2 << std::endl
+    }
+
+    std::cout << 'abc' << std::endl // Fordító függő az eredmény, definiálatlan a nyelv által
+
+    int j = 3.14;
+
+}
---
+++ 2021-22-1/porgnyelvek (C++)/jegyzet.md
@@ -0,0 +1,7 @@
+# Követelmény:
+Év végén egy beadandó ha sikerül akkor egy szóbeli vizsga adja a jegyet.
+
+# GY1
+
+Fordítás: g++ main.cpp
+Alapok
