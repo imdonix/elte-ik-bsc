ca63351e8aa66a17b53d58a5729cf7446eed3042
imdonix <tamas.donix@gmail.com>
2021-09-15 13:48:19 +0200

    cg gyak2 added

---
+++ 2021-22-1/cg/02_SDLEvents/02_SDLEvents/main.cpp
@@ -0,0 +1,166 @@
+﻿#include <SDL.h>
+#include <iostream>
+
+int main( int argc, char* args[] )
+{
+	// állítsuk be, hogy kilépés előtt hívja meg a rendszer az alábbi függvényt, ami a lambda törzsében található
+	atexit([] {
+			std::cout << "Press a key to exit the application..." << std::endl;
+			std::cin.get();
+		});
+
+	//
+	// 1. lépés: inicializáljuk az SDL-t
+	//
+
+	// a grafikus alrendszert kapcsoljuk csak be, ha gond van, akkor jelezzük és lépjün ki
+	if ( SDL_Init( SDL_INIT_VIDEO ) == -1 )
+	{
+		// irjuk ki a hibat es terminaljon a program
+		std::cout << "[SDL initialization] Error during the SDL initialization: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+			
+	//
+	// 2. lépés: hozzuk létre az ablakot, amire rajzolni fogunk
+	//
+
+	int height = 600;
+	int width = 800;
+
+	SDL_Window *win = nullptr;
+    win = SDL_CreateWindow( "Hello SDL!",				// az ablak fejléce
+							100,						// az ablak bal-felsõ sarkának kezdeti X koordinátája
+							100,						// az ablak bal-felsõ sarkának kezdeti Y koordinátája
+							width,						// ablak szélessége
+							height,						// és magassága
+							SDL_WINDOW_SHOWN);			// megjelenítési tulajdonságok
+
+	// ha nem sikerült létrehozni az ablakot, akkor írjuk ki a hibát, amit kaptunk és lépjünk ki
+    if (win == nullptr)
+	{
+		std::cout << "[Window creation] Error during the creation of an SDL window: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+	//
+	// 3. lépés: hozzunk létre egy renderelõt, rajzolót
+	//
+
+    SDL_Renderer *ren = nullptr;
+    ren = SDL_CreateRenderer(	win, // melyik ablakhoz rendeljük hozzá a renderert
+								-1,  // melyik indexú renderert inicializáljuka
+									 // a -1 a harmadik paraméterben meghatározott igényeinknek megfelelõ elsõ renderelõt jelenti
+								SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);	// az igényeink, azaz
+																						// hardveresen gyorsított és vsync-et beváró
+    if (ren == nullptr)
+	{
+        std::cout << "[Renderer creation] Error during the creation of an SDL renderer: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+	//
+	// 4. lépés: indítsuk el a fõ üzenetfeldolgozó ciklust
+	// 
+
+	// véget kell-e érjen a program futása?
+	bool quit = false;
+	// feldolgozandó üzenet ide kerül
+	SDL_Event ev;
+	// egér X és Y koordinátái
+	Sint32 mouseX = 0, mouseY = 0;
+	float rx = 0, ry = 0;
+	float velx = 250, vely = 250;
+
+	Uint32 last = SDL_GetTicks();
+	while (!quit)
+	{
+		// amíg van feldolgozandó üzenet dolgozzuk fel mindet:
+		while ( SDL_PollEvent(&ev) )
+		{
+			switch (ev.type)
+			{
+			case SDL_QUIT:
+				quit = true;
+				break;
+			case SDL_KEYDOWN:
+				if ( ev.key.keysym.sym == SDLK_ESCAPE )
+					quit = true;
+				break;
+			case SDL_MOUSEMOTION:
+				mouseX = ev.motion.x;
+				mouseY = ev.motion.y;
+				break;
+			case SDL_MOUSEBUTTONUP:
+				// egérgomb felengedésének eseménye; a felengedett gomb a ev.button.button -ban található
+				// a lehetséges gombok a következõek: SDL_BUTTON_LEFT, SDL_BUTTON_MIDDLE, 
+				//		SDL_BUTTON_RIGHT, SDL_BUTTON_WHEELUP, SDL_BUTTON_WHEELDOWN
+				break;
+			}
+		}
+
+		Uint32 current = SDL_GetTicks();
+		Uint32 deltaTick = current - last; 
+		float delta = deltaTick / (float) 1000;
+		last = current;
+
+		rx += velx * delta;
+		ry += vely * delta;
+
+		if (rx+20 > width || rx < 0)
+			velx *= -1;
+		if (ry+20 > height || ry < 0 )
+			vely *= -1;
+
+
+
+		// töröljük a hátteret fehérre
+		SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
+		SDL_RenderClear(ren);
+
+		// aktuális rajzolási szín legyen zöld és rajzoljunk ki egy vonalat
+		SDL_SetRenderDrawColor(	ren,	// renderer címe, aminek a rajzolási színét be akarjuk állítani
+								0,		// piros
+								255,	// zöld
+								0,		// kék
+								255);	// átlátszatlanság
+
+		SDL_RenderDrawLine(	ren,	// renderer címe, ahová vonalat akarunk rajzolni
+							0, 0, // vonal kezdõpontjának (x,y) koordinátái
+							mouseX, mouseY);// vonal végpontjának (x,y) koordinátái
+		
+		// definiáljunk egy (mouseX, mouseY) középpontó, tengelyekkel párhuzamos oldalú
+		// 20x20-as négyzetet:
+		SDL_Rect cursor_rect;
+		cursor_rect.x = rx;
+		cursor_rect.y = ry;
+		cursor_rect.w = 20;
+		cursor_rect.h = 20;
+		// legyen a kitöltési szín piros
+		SDL_SetRenderDrawColor( ren, 255, 0, 0, 255 );
+		SDL_RenderFillRect( ren, &cursor_rect);
+
+		// 1. feladat: az eltelt idõ függvényében periodikusan nõjjön és csökkenjen
+		//    az egérmutató középpontjával kirajzolt négyszög
+
+		// 2. feladat: ha a user a bal egérgombot nyomja meg akkor a téglalap színe váltson pirosra,
+		//    ha a jobb egérgombot, akkor kékre
+
+		// 3. beadható feladat: rajzolj ki egy 50 sugarú körvonalat az egérmutató köré!
+		// segítség: használd a SDL_RenderDrawLines()-t
+
+		// jelenítsük meg a backbuffer tartalmát
+		SDL_RenderPresent(ren);
+	}
+
+	//
+	// 4. lépés: lépjünk ki
+	// 
+
+	SDL_DestroyRenderer( ren );
+	SDL_DestroyWindow( win );
+
+	SDL_Quit();
+
+	return 0;
+}
---
+++ 2021-22-1/cg/szorgalmi.md
@@ -0,0 +1,8 @@
+Teames zip-elve kell beküldeni
+
+# 1 SDL-Eseménykezelés
+- Egérmutató körül keringjen egy kör (200 szakasz) (50 sugár) (100 keringés sugara) (5 másodpercenként)
+- Q -  keringés sugár csökken
+- E - keringés sugár növekszik
+- SPACE - megfordul a keringés + új színt (véletlen)
+- Határidő = 10.18 20:00
