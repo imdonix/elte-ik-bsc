c5aceca3de4e7bb9c982a355cced8603d915aea0
imdonix <tamas.donix@gmail.com>
2021-10-17 18:25:27 +0200

    ab II szakaszhossz alg added

---
+++ 2021-22-1/ab II/zh I/rle.js
@@ -0,0 +1,100 @@
+function binary(number)
+{
+    tmp = Array()
+    while(number > 0 || tmp.length == 0)
+    {
+        tmp.push(number % 2)
+        number = Math.floor(number / 2)
+    }
+    return tmp.reverse()
+}
+
+
+function decimal(binary)
+{
+    binary = binary.reverse()
+    let c = 0
+    for (const i in binary) 
+        if(binary[i] == 1)
+            c += Math.pow(2, i)
+    return c
+}
+
+function str2binary(input)
+{
+    tmp = Array()
+    for (let i = 0; i < input.length; i++)
+        tmp.push(input.charAt(i))
+    return tmp
+}
+
+function binary2str(input)
+{
+    str = ''
+    for (const bit of input)
+        str += bit
+    return str
+}
+
+function encode(input)
+{
+    input = str2binary(input)
+
+    let ls = new Array()
+    let c = 0
+    for (const bit of input)
+    {
+        if(bit == 1)
+        {
+            ls.push(c)
+            c = 0
+        }
+        else
+            c++
+    }
+
+    let res = Array()
+    rn = ls.map(binary)
+    for (const r of rn) 
+    {
+
+        let l = r.length - 1
+        let sub = Array(l).fill(1)
+        sub.push(0)
+        sub.push(...r)
+        res.push(...sub)
+    }
+
+    return [res, ls, rn]
+}
+
+function decode(input)
+{
+    input = str2binary(input)
+
+    let ls = new Array()
+    let c = 0
+    for (let i = 0; i < input.length; i++) 
+    {
+        if(input[i] == 0)
+        {
+            let sub = Array()
+            for (let j = 0; j < c + 1; j++, i++) 
+                sub.push(input[i+1])
+            ls.push(sub)
+            c = 0
+        }
+        else
+            c++
+    }
+
+    let res = Array()
+    let rn = ls.map(decimal)
+    for (const n of rn)
+    {
+        res.push(...Array(n).fill(0))
+        res.push(1)
+    }
+
+    return [res, ls, rn]
+}
